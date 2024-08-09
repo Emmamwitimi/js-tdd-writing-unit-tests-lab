30e43017355e1dcbc844b5ba2fd7173a9995c202
Emmaculate <emmamwitimi14@gmail.com>
2024-08-09 17:56:41 +0300

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,12 @@
 // Your tests here
+import { isPalindrome } from '../utils';
+
+describe('isPalindrome', () => {
+  test('returns true for "racecar"', () => {
+    expect(isPalindrome('racecar')).toBe(true);
+  });
+
+  test('returns false for "car"', () => {
+    expect(isPalindrome('car')).toBe(false);
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
 // Your code here
+export function isPalindrome(word) {
+    if (typeof word !== 'string') {
+      throw new Error('Input must be a string');
+    }
+  
+    if (!/^[A-Za-z]+$/.test(word)) {
+      throw new Error('Invalid input');
+    }
+  
+    const cleanedWord = word.toLowerCase();
+    const reversedWord = cleanedWord.split('').reverse().join('');
+    return cleanedWord === reversedWord;
+  }
